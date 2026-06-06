1c26b95d16b3e55564b1642cf766ec5bf64cbeab
nikjavor <nik.javor+git@proton.me>
2026-06-06 20:31:22 +0200

    change pins

--- scorpy-board/include/board_pins.h
+++ scorpy-board/include/board_pins.h
@@ -1,11 +1,11 @@
 #pragma once
 
-constexpr int PIN_HOME = 25;
-constexpr int PIN_AWAY = 26;
-constexpr int PIN_PAIR = 27;
-constexpr int PIN_LED = 32;
+constexpr int PIN_HOME = 32;
+constexpr int PIN_AWAY = 33;
+constexpr int PIN_PAIR = 13;
+constexpr int PIN_LED = 27;
 
-constexpr int PIN_SERVO_HO = 4;
-constexpr int PIN_SERVO_HT = 18;
-constexpr int PIN_SERVO_AO = 19;
+constexpr int PIN_SERVO_HO = 18;
+constexpr int PIN_SERVO_HT = 19;
+constexpr int PIN_SERVO_AO = 22;
 constexpr int PIN_SERVO_AT = 23;
